80603a444ac961a2fee6fa283d5a49f9b9a07852
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-04-16 23:06:12 +0530

    Added Home Page

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Revolutionizing Web Development</h2>
+      <p>We provide cutting-edge solutions for your digital presence.</p>
+    </div>
+  );
+};
+
+export default Home;
