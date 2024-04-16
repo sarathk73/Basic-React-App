71e6a4d8dbfe03008031587b0bbebd49c37aa2c7
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-04-16 23:04:00 +0530

    Added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2024 Hashterisk. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
