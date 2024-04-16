85c444cd0b19ca9065c2a52b4e7ef8b47b5d2bcf
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-04-16 23:05:47 +0530

    Added Services Page

---
+++ src/pages/Services.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div>
+      <h2>Our Services</h2>
+      <ul>
+        <li>Web Development</li>
+        <li>App Development</li>
+        <li>Technology Consulting</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Services;
