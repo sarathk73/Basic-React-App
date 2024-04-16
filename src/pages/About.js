2e629834c61a397e597f50d3c6cd442859eea5da
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-04-16 23:05:02 +0530

    Added About Section

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>We are a team dedicated to providing top-notch web services.</p>
+    </div>
+  );
+};
+
+export default About;
