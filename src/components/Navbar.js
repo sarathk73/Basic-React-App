34dab45e799591192c6d4039e7c15981f04ff71b
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-04-16 23:04:18 +0530

    Added NavBar

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/services">Services</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
