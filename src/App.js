0eacb5839842a72d30375f2707d7d18560cd2920
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-04-17 20:01:59 +0530

    Added layout to App

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import './App.css'; // Make sure this import points to your global stylesheet
+   // src/App.js
+   import React from 'react';
+   import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+   import Layout from './components/Layout';
+   import Home from './pages/Home';
+   import Services from './pages/Services';
+   import About from './pages/About';
+   import Contact from './pages/Contact';
 
-const App = () => {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            {/* Add more routes here */}
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
-};
+   const App = () => {
+     return (
+       <Router>
+         <Layout>
+           <Routes>
+             <Route path="/" element={<Home />} />
+             <Route path="/services" element={<Services />} />
+             <Route path="/about" element={<About />} />
+             <Route path="/contact" element={<Contact />} />
+           </Routes>
+         </Layout>
+       </Router>
+     );
+   };
 
-export default App;
+   export default App;
